693c8e0e274ae8e4fcafe5c7939bc3137cd1dfc6
daluga0503 <daniel.luquegallardo@gmail.com>
2022-11-08 10:45:45 +0100

    24 added

--- Ejercicio24.java/Ejercicio24.java
+++ Ejercicio24.java/Ejercicio24.java
@@ -7,37 +7,56 @@ public class Ejercicio24 {
         System.out.println("2. Programador senior.");
         System.out.println("3. Jefe de proyecto.");
 
+        int sueldobase=0;
+
         System.out.print("Introduzca el cargo(1-3): ");
         int cargo = sc.nextInt();
 
         System.out.print("¿Cuántos dias has estado de viaje visitando clientes: ");
         int dias = sc.nextInt();
+        int dietas=dias*30;
 
         System.out.println("Introduzca su estado civil (1- Soltero, 2- Casado):");
         int estado = sc.nextInt();
-
+        double irpf=0;
 
         switch (cargo) {
             case 1:
-                int sueldo_base_1 = 950;
-                int dietas = dias*30;
-                int sueldo_bruto_1 = sueldo_base_1+(dias*30);
-                System.out.printf("Sueldo base: %10s \n", sueldo_base_1);
-                System.out.printf("Dietas (%d viajes)  %.2.f \n", dias, dietas);
-                System.out.printf("Sueldo bruto: %10s \n", sueldo_bruto_1);
-                
+                sueldobase=950;
                 break;
-
-            case 2:
-                
+                case 2:
+                sueldobase=1200;
                 break;
-
-            case 3:
-                
+                case 3:
+                sueldobase=1600;
                 break;
         
             default:
                 break;
         }
+
+        double sueldobruto=sueldobase+dietas;
+
+
+
+        if (estado == 1) { // Soltero
+            irpf = 25;
+          } else if (estado == 2) { // Casado
+            irpf = 20;
+            } else {
+            System.out.println("No ha elegido correctamente el estado civil.");
+            }
+
+            double cuantiaIrpf = (sueldobruto * irpf) / 100;
+            double sueldoneto=sueldobruto-cuantiaIrpf;
+
+            System.out.printf("Sueldo base: %16s\n", sueldobase);
+            System.out.printf("Dietas (%d viajes): %10s\n", dias,dietas);
+            System.out.printf("Sueldo bruto: %15s\n", sueldobruto);
+            System.out.printf("Retención IRPF (%.0f):   %.2f\n",irpf,cuantiaIrpf);
+            System.out.printf("Sueldo neto:          %.2f ", sueldoneto);
+
+        
+        sc.close();
     }
 }
